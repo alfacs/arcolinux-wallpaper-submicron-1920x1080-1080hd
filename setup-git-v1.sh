e163076c45457427c30f7f1dc8827319b584b13a
Alexey Bobkov <bobkov.compro@gmail.com>
2018-09-11 15:22:00 +0300

    добавление административных файлов репозитория

--- setup-git-v1.sh
+++ setup-git-v1.sh
@@ -13,7 +13,7 @@ set -e
 
 git init
 git config --global user.name "Alexey Bobkov"
-git config --global user.email "alfacs@yandex.ru"
+git config --global user.email "bobkov.compro@gmail.com"
 sudo git config --system core.editor nano
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=25000'
