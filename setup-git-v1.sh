3abe54ff96e34685c324d38dffb9f1165173f8ac
Alexey Bobkov <alfacs@yandex.ru>
2018-09-11 15:13:16 +0300

    adding administration files

---
+++ setup-git-v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Alexey Bobkov
+# Website	:	https://www.compro.ru
+# Website	:	https://www.compro.by
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+# change into your name and email.
+
+git init
+git config --global user.name "Alexey Bobkov"
+git config --global user.email "alfacs@yandex.ru"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
